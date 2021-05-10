f7b86dba7753c65e434f6933e4f5445913a31d44
Gerald Andrei Tamasi <tamasigerald@gmail.com>
2021-05-10 15:02:11 +0200

    added basic login form + fixed styles

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,13 +1,38 @@
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
-import ThemeButton from 'Components/ThemeButton';
 import Layout from 'Components/Layout';
 
+type FormData = {
+    username: string;
+    password: string;
+};
+
 const LoginPage: FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<FormData>();
+    const onSubmit = handleSubmit((data) => console.log(data));
     return (
         <Layout>
-            Login page
-            <ThemeButton />
+            <form className="form form--login" onSubmit={onSubmit}>
+                <h1 className="form__title">Login:</h1>
+                <input
+                    className="input"
+                    placeholder="username"
+                    {...register('username', { required: true })}
+                />
+                {errors.username && <span>Username required</span>}
+                <input
+                    className="input"
+                    placeholder="password"
+                    {...register('password', { required: true })}
+                />
+                {errors.password && <span>Password required</span>}
+                <button className="btn btn--success">Submit</button>
+            </form>
         </Layout>
     );
 };
